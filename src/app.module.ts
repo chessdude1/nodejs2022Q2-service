2b74060429d1ab7f08ea906a141853e22f68763f
chessdude1 <mitia.platonow2011@yandex.ru>
2022-07-31 22:58:02 +0300

    feat: add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+import { UserTestModule } from './UserTEST/userTest.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlbumsModule } from './Albums/albums.module';
 import { ArtistsModule } from './Artists/artists.module';
+import configService from './config/config.service';
 import { FavoritesModule } from './Favorites/favorites.module';
 import { TracksModule } from './Tracks/tracks.module';
 import { UserModule } from './user/user.module';
@@ -12,6 +15,8 @@ import { UserModule } from './user/user.module';
     ArtistsModule,
     AlbumsModule,
     FavoritesModule,
+    UserTestModule,
+    TypeOrmModule.forRoot(configService),
   ],
 })
 export class AppModule {}
